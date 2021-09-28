c82eaf1686f8ada76e04251d1a600cbba927bba5
Idris Raja <idris.raja@gmail.com>
2021-09-28 10:00:40 -0500

    fun

---
+++ graham/3-11.hs
@@ -0,0 +1,27 @@
+
+add :: Int -> Int -> Int -> Int
+add x y z = x + y + z
+
+copy :: a -> (a, a)
+copy a = (a, a)
+
+apply :: (a -> b) -> a -> b
+apply x y = x y
+
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x, y)
+
+double :: Num a => a -> a
+double x = x*2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a->a) -> a -> a
+twice f x = f (f x)
